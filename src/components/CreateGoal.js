cbac3e110fffe4e0d484e2d5d321a70a5fd8fa93
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2019-02-04 16:43:57 +0100

    add simple submit form, event handlers and state

--- src/components/CreateGoal.js
+++ src/components/CreateGoal.js
@@ -1,15 +1,47 @@
+/*
+[ ] - Display box on click
+    [ ] - Title
+[ ] - save goal to array
+*/
+
 import React from 'react';
 
 class CreateGoal extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            newGoal: ''
+        };
 
+    }
 
+    newTodoChanged(e) {
+                this.setState({
+            newGoal: e.target.value
+        })
+    }
+
+    submitGoal(e) {
+        e.preventDefault();
+        console.log(this.state.newGoal);
+    }
 
 
     render() {
         return(
-            <div className="create-goal-btn">
-                    <button className="btn-primary btn-large">Create New Goal</button>
+                
+            <div className="center-padded">
+
+                <form onSubmit={(e) => this.submitGoal(e)}>
+                    <label htmlFor="NewGoal">Goal Title:</label>
+                    <input onChange={(e) => this.newTodoChanged(e)} id="newGoal" name="newGoal" />
+                    <button type="submit" className="btn-primary btn-large">
+                        Create New Goal
+                    </button>
+                </form>
+
             </div>
+
         );
     }
 }
