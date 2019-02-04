163b4336c496b0824396106387542500473107c3
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2019-02-04 11:49:37 +0100

    add static navbar

--- src/components/Goals.js
+++ src/components/Goals.js
@@ -1,29 +1,30 @@
 /*
 [ ] - Static Mockup
     [ ] - create a background
+    [x] - create a navbar
     [ ] - render the page
     [ ] - create a navbar
     [ ] - create a table like structure for goals
 
 */
 
+import {Nav, Navbar} from 'react-bootstrap';
 import React from 'react';
 
 class Goals extends React.Component{
-    constructor(props){
-        super(props);
-    }
 
     render() {
-
       return (
-          <div>Hello world!</div>
+          <Navbar bg="dark" variant="dark">
+            <Navbar.Brand href="#home">Goals</Navbar.Brand>
+            <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#Progress">Progress</Nav.Link>
+            <Nav.Link href="#settings">Settings</Nav.Link>
+            </Nav>
+        </Navbar>
       );
     }
-
-    
 }
-
-
 
 export default Goals;
