1821bb7573a7520271468426ea4b9e2143f91d00
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2019-02-04 13:56:36 +0100

    setup Navbar header

--- src/components/Goals.js
+++ src/components/Goals.js
@@ -2,29 +2,39 @@
 [ ] - Static Mockup
     [ ] - create a background
     [x] - create a navbar
-    [ ] - render the page
-    [ ] - create a navbar
-    [ ] - create a table like structure for goals
+    [x] - render the page
+    [ ] - create a table like structure for goals. rows = [goal text - wide][checkbox - small]
 
 */
 
 import {Nav, Navbar} from 'react-bootstrap';
+import GoalRows from './GoalRow';
 import React from 'react';
+
 
 class Goals extends React.Component{
 
     render() {
       return (
-          <Navbar bg="dark" variant="dark">
-            <Navbar.Brand href="#home">Goals</Navbar.Brand>
-            <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#Progress">Progress</Nav.Link>
-            <Nav.Link href="#settings">Settings</Nav.Link>
-            </Nav>
-        </Navbar>
+        <div>
+            <Navbar bg="dark" variant="dark">
+                <Navbar.Brand href="#home">Goals</Navbar.Brand>
+                <Nav className="mr-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#Progress">Progress</Nav.Link>
+                <Nav.Link href="#settings">Settings</Nav.Link>
+                </Nav>
+            </Navbar>
+
+            <GoalRows />
+
+        </div>
+
       );
     }
+
+
+
 }
 
 export default Goals;
