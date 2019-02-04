5379fbbbe61c310eb1d2aa96d639d4c54aee905b
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2019-02-04 14:50:39 +0100

    add CreateGoal component - Handles creation of new goal

---
+++ src/components/CreateGoal.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class CreateGoal extends React.Component{
+
+
+
+
+    render() {
+        return(
+            <div className="create-goal-btn">
+                    <button className="btn-primary btn-large">Create New Goal</button>
+            </div>
+        );
+    }
+}
+
+export default CreateGoal;
