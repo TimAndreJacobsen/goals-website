454dc9e1ea8af08909018391858845e5ae516020
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2019-02-04 10:37:12 +0100

    basic structure
    Goals is an SPA which lets you set your own goals.
    A goal can be anything. ex: remember to do X, or spend Y time doing Z every day.
    It can be used to train habits or as a ToDo app. What makes this app different?
    The focus is to provide you with detailed feedback on how you are achieving your goals. Let you specify time, goal achievement and visualize data metrics on your progression.

---
+++ src/components/Goals.js
@@ -0,0 +1,20 @@
+/*
+[ ] - Static Mockup
+    [ ] - create a background
+    [ ] - render the page
+    [ ] - create a navbar
+    [ ] - create a table like structure for goals
+
+*/
+
+import React from 'react';
+
+class Goals extends React.Component{
+    constructor(props){
+        super(props);
+    }
+}
+
+
+
+export default Goals;
